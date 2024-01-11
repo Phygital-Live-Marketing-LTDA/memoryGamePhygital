c64ac608341580d0ed39ae94ad17ab2293991e71
PedroTresmondi <pedro_tresmondi@hotmail.com>
2024-01-11 12:43:41 -0300

    revertendo mudanças

--- src/api/server.js
+++ src/api/server.js
@@ -20,10 +20,7 @@ const db = new sqlite3.Database('../data/leads.db', (err) => {
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           name TEXT NOT NULL,
           email TEXT NOT NULL,
-<<<<<<< HEAD
           points INTEGER DEFAULT 0,
-=======
->>>>>>> 3178bd4a122e4652e62273dbaa923b3703e74ddd
           started_at TIMESTAMP DEFAULT (datetime('now', 'localtime'))
         )
       `, [], (err) => {
@@ -40,25 +37,14 @@ const db = new sqlite3.Database('../data/leads.db', (err) => {
 
 // Rota para adicionar um novo lead
 app.post('/leads', (req, res) => {
-<<<<<<< HEAD
     const { name, email, points } = req.body;
-=======
-    const { name, email } = req.body;
-    console.log('Received lead:', req.body);
-
->>>>>>> 3178bd4a122e4652e62273dbaa923b3703e74ddd
 
     if (!name || !email) {
         return res.status(400).send('Name and email are required');
     }
 
-<<<<<<< HEAD
     const stmt = db.prepare('INSERT INTO leads (name, email, points) VALUES (?, ?, ?)');
     stmt.run(name, email, points, function (err) {
-=======
-    const stmt = db.prepare('INSERT INTO leads (name, email) VALUES (?, ?)');
-    stmt.run(name, email, function (err) {
->>>>>>> 3178bd4a122e4652e62273dbaa923b3703e74ddd
         if (err) {
             return res.status(500).send('Error inserting lead into database');
         }
@@ -67,7 +53,6 @@ app.post('/leads', (req, res) => {
     });
 });
 
-<<<<<<< HEAD
 // Rota para atualizar pontos de um lead
 app.put('/leads', (req, res) => {
     const { name, points } = req.body;
@@ -86,8 +71,6 @@ app.put('/leads', (req, res) => {
     });
 });
 
-=======
->>>>>>> 3178bd4a122e4652e62273dbaa923b3703e74ddd
 // Inicia o servidor
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
--- src/pages/InitialPage.js
+++ src/pages/InitialPage.js
@@ -1,63 +1,31 @@
-// SplashScreen.js
-import LeadForm from '../components/LeadForm';
-import MemoryGame from '../components/Game/MemoryGame';
-import '../App.css';
+import React, { useState } from 'react';
+import LeadCapture from '../components/LeadCapture/LeadCapture';
 
-function InitialPage() {
-const [showForm, setShowForm] = useState(false);
-const [gameStarted, setGameStarted] = useState(false);
+function InitialPage({ onStartGame }) {
+    const [showLeadCapture, setShowLeadCapture] = useState(false);
 
-const handleClick = () => {
-setShowForm(true);
-};
+    // Função para lidar com o início do jogo após o envio bem-sucedido dos dados do lead
+    const handleStartGameWithLead = (lead) => {
+        // Chama a função onStartGame que foi passada pelo componente pai (App.js) com os dados do lead
+        onStartGame(lead);
+    };
 
-const handleLeadSubmit = async ({ name, email }) => {
-const leadData = { name, email };
-try {
-const response = await fetch('http://localhost:3001/leads', {
-method: 'POST',
-headers: {
-'Content-Type': 'application/json',
-},
-body: JSON.stringify(leadData),
-});
+    const handleScreenClick = () => {
+        setShowLeadCapture(true);
+    };
 
-  if (response.ok) {
-    console.log('Lead saved');
-    setGameStarted(true); // Indica que o jogo deve ser iniciado
-  } else {
-    console.error('Failed to save lead');
-  }
-} catch (error) {
-  console.error('Error:', error);
-}
-};
-
-if (gameStarted) {
-return <MemoryGame />
-}
-
-return (
-<div className='bg-black h-screen flex flex-col justify-center items-center' onClick={handleClick}>
-{!showForm && (
-<>
-<div className='flex justify-center mb-4'>
-<img className='animate pop delay' src='https://www.figma.com/file/V5EZ6XpHsPav7dULbSlWVX/image/d08184b272a3eefdfa1cbefb0749a75fd33f4b22' alt='logo da empresa Phygital Lab' />
-</div>
-<div className='flex items-center justify-center gap-2 '>
-<span className="relative flex h-3 w-3 animate pop delay-1">
-<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
-<span className="relative inline-flex rounded-full h-3 w-3 bg-white/80"></span>
-</span>
-<p className='text-white font-light animate pop delay-2 '>Toque em qualquer área da tela para iniciar</p>
-</div>
-</>
-)}
-{showForm && (
-<LeadForm onSubmit={handleLeadSubmit} />
-)}
-</div>
-);
+    return (
+        <div className='bg-black h-screen flex flex-col justify-center items-center' onClick={!showLeadCapture ? handleScreenClick : null}>
+            <div className='flex justify-center mb-4'>
+                <img src='https://www.figma.com/file/V5EZ6XpHsPav7dULbSlWVX/image/d08184b272a3eefdfa1cbefb0749a75fd33f4b22' alt='logo da empresa Phygital Lab' />
+            </div>
+            {!showLeadCapture ? (
+                <p className='text-white font-light'>Toque em qualquer área da tela para iniciar</p>
+            ) : (
+                <LeadCapture onStartGame={handleStartGameWithLead} />
+            )}
+        </div>
+    );
 }
 
 export default InitialPage;
