58c72b0c41fe706cc936f9fe78c19a67dcd71ad5
PedroTresmondi <pedro_tresmondi@hotmail.com>
2024-01-10 21:02:10 -0300

    Add a lead capture form to the splash screen

--- src/pages/InitialPage.js
+++ src/pages/InitialPage.js
@@ -1,20 +1,30 @@
 // SplashScreen.js
-import React from 'react';
+import React, { useState } from 'react';
 import LeadCapture from '../components/LeadCapture/LeadCapture';
 
 function SplashScreen({ onStartGame }) {
+    const [showLeadCapture, setShowLeadCapture] = useState(false);
+
+    const handleScreenClick = () => {
+        setShowLeadCapture(true); // This will trigger the LeadCapture form to show
+    };
+
     return (
-        <div className='bg-black h-screen flex flex-col justify-center items-center' onClick={() => onStartGame()}>
+        <div className='bg-black h-screen flex flex-col justify-center items-center' onClick={!showLeadCapture ? handleScreenClick : null}>
             <div className='flex justify-center mb-4'>
-            <img src='https://www.figma.com/file/V5EZ6XpHsPav7dULbSlWVX/image/d08184b272a3eefdfa1cbefb0749a75fd33f4b22' alt='logo da empresa Phygital Lab' />
+                <img src='https://www.figma.com/file/V5EZ6XpHsPav7dULbSlWVX/image/d08184b272a3eefdfa1cbefb0749a75fd33f4b22' alt='logo da empresa Phygital Lab' />
             </div>
-            <div className='flex items-center justify-center gap-2'>
-                <span className="relative flex h-3 w-3 animate pop delay-1">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-white/80"></span>
-                </span>
-                <p className='text-white font-light animate pop delay-2'>Toque em qualquer área da tela para iniciar</p>
-            </div>
+            {!showLeadCapture ? (
+                <div className='flex items-center justify-center gap-2'>
+                    <span className="relative flex h-3 w-3 animate pop delay-1">
+                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-3 w-3 bg-white/80"></span>
+                    </span>
+                    <p className='text-white font-light animate pop delay-2'>Toque em qualquer área da tela para iniciar</p>
+                </div>
+            ) : (
+                <LeadCapture onStartGame={onStartGame} />
+            )}
         </div>
     );
 }
