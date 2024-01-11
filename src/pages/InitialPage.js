df4aa1a246c3587646daf178db0842bce6adb7ca
PedroTresmondi <pedro_tresmondi@hotmail.com>
2024-01-11 12:53:36 -0300

    Added CSS animations and keyframes to the

--- src/pages/InitialPage.js
+++ src/pages/InitialPage.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import LeadCapture from '../components/LeadCapture/LeadCapture';
+import "../App.css"
+
 
 function InitialPage({ onStartGame }) {
     const [showLeadCapture, setShowLeadCapture] = useState(false);
 
-    // Função para lidar com o início do jogo após o envio bem-sucedido dos dados do lead
     const handleStartGameWithLead = (lead) => {
-        // Chama a função onStartGame que foi passada pelo componente pai (App.js) com os dados do lead
         onStartGame(lead);
     };
 
@@ -17,11 +17,17 @@ function InitialPage({ onStartGame }) {
     return (
         <div className='bg-black h-screen flex flex-col justify-center items-center' onClick={!showLeadCapture ? handleScreenClick : null}>
             <div className='flex justify-center mb-4'>
-                <img src='https://www.figma.com/file/V5EZ6XpHsPav7dULbSlWVX/image/d08184b272a3eefdfa1cbefb0749a75fd33f4b22' alt='logo da empresa Phygital Lab' />
+                <img className='animate pop delay' src='https://www.figma.com/file/V5EZ6XpHsPav7dULbSlWVX/image/d08184b272a3eefdfa1cbefb0749a75fd33f4b22' alt='logo da empresa Phygital Lab' />
+            </div>
+            <div className='flex items-center justify-center gap-2 '>
+                <span className="relative flex h-3 w-3 animate pop delay-1">
+                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
+                    <span className="relative inline-flex rounded-full h-3 w-3 bg-white/80"></span>
+                </span>
+
             </div>
             {!showLeadCapture ? (
-                <p className='text-white font-light'>Toque em qualquer área da tela para iniciar</p>
-            ) : (
+                <p className='text-white font-light animate pop delay-2 '>Toque aqui para iniciar</p>) : (
                 <LeadCapture onStartGame={handleStartGameWithLead} />
             )}
         </div>
