e48ed2cdfd56ec999ea9bbedc76df7bbc4227eb1
PedroTresmondi <pedro_tresmondi@hotmail.com>
2024-01-16 13:54:10 -0300

    Add Tailwind CSS to the project and update the InitialPage component.

--- src/pages/InitialPage.js
+++ src/pages/InitialPage.js
@@ -15,21 +15,15 @@ function InitialPage({ onStartGame }) {
     };
 
     return (
-        <div className='bg-black h-screen flex flex-col justify-center items-center' onClick={!showLeadCapture ? handleScreenClick : null}>
-            <div className='flex justify-center mt-10'>
-                <img className='animate pop delay' src='https://www.figma.com/file/V5EZ6XpHsPav7dULbSlWVX/image/d08184b272a3eefdfa1cbefb0749a75fd33f4b22' alt='logo da empresa Phygital Lab' />
-            </div>
-            <div className='flex items-center justify-center gap-2 '>
-                <span className="relative flex h-3 w-3 animate pop delay-1">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-white/80"></span>
-                </span>
-
-            </div>
-            {!showLeadCapture ? (
-                <p className='text-white font-light animate pop delay-2 '>Toque aqui para iniciar!</p>) : (
-                <LeadCapture onStartGame={handleStartGameWithLead} />
+        <div className=' h-screen flex flex-col justify-center items-center' onClick={!showLeadCapture ? handleScreenClick : null}>
+            { !showLeadCapture && (
+                <>
+                    <img className='animate pop delay' src='https://www.figma.com/file/V5EZ6XpHsPav7dULbSlWVX/image/d08184b272a3eefdfa1cbefb0749a75fd33f4b22' alt='logo da empresa Phygital Lab' />
+                    <p className='text-white 
+                    font-light mt-5 animate pop delay-2 '>Clique em qualquer lugar da tela!</p>
+                </>
             )}
+            { showLeadCapture && <LeadCapture onStartGame={handleStartGameWithLead} /> }
         </div>
     );
 }
